5fc6aa54dfe4b87fd5354c1966f8fad57dcddf0c
Silas17 <silascs120@gmail.com>
2026-01-16 08:04:55 -0300

    att3

--- Desafio01/index.js
+++ Desafio01/index.js
@@ -1,20 +1,26 @@
 let nome = "player1";
 let xpLevel = 5213;
 
-if (xpLevel < 1000) {
-  console.log(`${nome} esta no Ferro`);
-} else if (xpLevel >= 1001 && xpLevel < 2000) {
-  console.log(`${nome} esta no Bronze`);
-} else if (xpLevel >= 2001 && xpLevel < 5000) {
-  console.log(`${nome} esta no Prata`);
-} else if (xpLevel >= 5001 && xpLevel < 7000) {
-  console.log(`${nome} esta no Ouro`);
-} else if (xpLevel >= 7001 && xpLevel < 8000) {
-  console.log(`${nome} esta no Platina`);
-} else if (xpLevel >= 8001 && xpLevel < 9000) {
-  console.log(`${nome} esta no Ascendente`);
-} else if (xpLevel >= 9001 && xpLevel < 10000) {
-  console.log(`${nome} esta no Imortal`);
-} else if (xpLevel >= 10001) {
-  console.log(`${nome} esta no Radiante`);
-}
+switch (true) {
+  let menssagem = `O jogador ${nome} possui ${xpLevel} pontos de experiência e é da categoria ${rank}`;
+  case xpLevel < 1000:
+    let rank = `Ferro`;
+    console.log(menssagem);
+  case xpLevel >= 1001 && xpLevel <= 2000:
+    let rank = `Bronze`;
+    console.log(menssagem);
+  case xpLevel >= 2001 && xpLevel <= 3000:
+    let rank = `Prata`;
+    console.log(menssagem);
+  case xpLevel >= 3001 && xpLevel <= 4000:
+    let rank = `Ouro`;
+    console.log(menssagem);
+  case xpLevel >= 4001 && xpLevel <= 5000:
+    let rank = `Platina`;
+    console.log(menssagem);
+  case xpLevel > 5000:
+    let rank = `Diamante`;
+    console.log(menssagem);
+
+  }
+
