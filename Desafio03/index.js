9047cb39a7a5312bf0e09c8280afb2be791db48e
Silas17 <silascs120@gmail.com>
2026-01-28 10:17:34 -0300

    Function-Class

--- Desafio03/index.js
+++ Desafio03/index.js
@@ -1,9 +1,23 @@
+let player = {
+    classType: {0: "Warrior", 1: "Ranger", 2: "Mage", 3: "Ninja"},
+    }
+
+function createPlayer(name, age, classType) {
+    if (classType in player.classType) {
+        return new Player(name, age, player.classType[classType]);
+    } else {
+        return "Invalid class type. Please choose a valid class type.";
+        }
+    }
+
 class Player {
     constructor(name, age, classType) {
         this.name = name;
         this.age = age;
         this.classType = classType;
     } 
+    
 }
-let Player1 = new Player("Arthur", 25, "Guerreiro");
-console.log(Player1);
+
+let newPlayer = {player0:  createPlayer("Aragorn", 87, 0)};
+console.log(newPlayer.player0);
