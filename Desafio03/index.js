590c514802cbb6a8e98d1f93ae5698c6a250e3b1
Silas17 <silascs120@gmail.com>
2026-01-28 09:39:41 -0300

    Create-Class

---
+++ Desafio03/index.js
@@ -0,0 +1,9 @@
+class Player {
+    constructor(name, age, classType) {
+        this.name = name;
+        this.age = age;
+        this.classType = classType;
+    } 
+}
+let Player1 = new Player("Arthur", 25, "Guerreiro");
+console.log(Player1);
